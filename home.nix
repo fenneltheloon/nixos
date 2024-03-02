26ba1f9fc991bd43e4ff07dc8927ddef69a53a0f
Fennel <fennel@everwild.dev>
2024-03-02 15:58:06 -0500

    syncthing

--- home.nix
+++ home.nix
@@ -109,7 +109,7 @@
     settings = {
       character = {
         success_symbol = ''[\([ᴖ](bold red) ᵥ [ᴖ](bold red)\)](bold bg:black)'';
-	error_symbol = ''[\([ᴗ](bold red) ᵥ [ᴗ](bold red)\)](bold bg:black)'';
+	    error_symbol = ''[\([ᴗ](bold red) ᵥ [ᴗ](bold red)\)](bold bg:black)'';
       };
     };
   };
@@ -146,6 +146,11 @@
     '';
   };
 
+  services.syncthing = {
+  	enable = true;
+  	extraOptions = [];
+  };
+
 #  programs.hyfetch = {
 #    enable = true;
 #    settings = {
@@ -158,37 +163,6 @@
 #      };
 #    };
 #  };
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/fennel/etc/profile.d/hm-session-vars.sh
-  #
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
